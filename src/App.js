bba1448ba3a85c0ad921aeec184decff92a8065d
jannathulks2007 <jannathulks2007@gmail.com>
2026-08-03 16:22:03 +0530

    mytodo form developed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ToDo from './ToDo';
 function App() {
   return (
     <div className="App">
-
+      <div>
+        <ToDo/>
+      </div>
     </div>
   );
 }
